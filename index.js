183576b6c79e6670d2e373c1403ef7dc1a1cf572
carlo-baron <carlobaron430@gmail.com>
2024-12-05 18:34:46 +0800

    Finished
    Not the best design and code but it works for me

---
+++ index.js
@@ -0,0 +1,35 @@
+account_size = document.getElementById("size");
+risk_amount = document.getElementById("risk");
+stop_loss = document.getElementById("sl");
+
+results = document.getElementsByName("result");
+position_size_field = results[0];
+leverage_field = results[1];
+
+leverage = 1;
+
+document.getElementById("submit").onclick = function(e){
+    e.preventDefault();
+    try{
+        divisor = parseFloat(stop_loss.value) / 100;
+        position_size = parseFloat(risk_amount.value) / divisor;
+        console.log(position_size);
+
+        float_size = parseFloat(account_size.value);
+        if(position_size > float_size){
+            leverage = position_size / parseFloat(float_size);
+            console.log(leverage);
+            console.log(account_size.value);
+
+        }
+
+        position_size_field.value = position_size.toFixed(2);
+        leverage_field.value = leverage.toFixed(2);
+
+    }catch(e){
+        console.error(e)
+    }
+
+    
+
+}
